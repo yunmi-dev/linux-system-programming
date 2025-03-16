b947d24974552ba31aaf786ba1bf93bbe09e7856
Yunmi Jeong <yunmiii0226@khu.ac.kr>
2025-03-16 14:00:12 +0000

    Week 2: Add C programming basics exercises and documentation - Add practice code (hello.c, output.c, input.c, args.c, string.c) - Create Makefile for build automation - Create README with detailed explanation of C language fundamentals - Document common issues such as gets() function security vulnerability

--- labs/02-quick-introduction-to-C/input.c
+++ labs/02-quick-introduction-to-C/input.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
-int main() {
-	char c ,s[80];
+int main()
+{
+	char c, s[80];
 	int i; long l;
 	float f; double d;
-
-	scanf("%c %s %d %ld %f %lf", &c, s, &i, &l, &f, &d);
-	printf("Output: %c %s %d %ld %.4f %.2lf\n", c, s, i, l, f, d);
+	
+	scanf("%c %s %d %ld %f %lf", &c, s, &i, &l, &f, &d); 
+	printf("Output: %c %s %d %ld %.4f %.2lf\n", c, s, i, l, f, d); 
 
 	c = getchar();
 	putchar(c);
 
-	fgets(s, sizeof(s), stdin);
+	gets(s);
+	// 주의: gets() 함수는 보안상 취약점이 있어 현대 C 프로그래밍에서는 권장X
+	// 학습 목적으로만 사용하며, 실제 개발에서는 fgets()를 사용하는 것이 좋다. (컴파일 시에도 warning)
 	puts(s);
 
 	return 0;
--- labs/02-quick-introduction-to-C/output.c
+++ labs/02-quick-introduction-to-C/output.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	char c = 'a', s[] = "hello";
 	int i = 100; long l = 99999;
 	float f = 3.14; double d = 99.999;
 	int *p = &i;
-
-	printf("Output: %c %s %d %#X %ld %.4f %.2lf %p\n", c, s, i, i, l, f, d, p);
+	
+	printf("Output: %c %s %d %#X %ld %.4f %.2lf %p\n", c, s, i, i, l, f, d, p); 
 	putchar(c);
 	puts(s);
 
