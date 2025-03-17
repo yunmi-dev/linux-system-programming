543c52a1cda3a88b4c5b85c1ac5946ff8a8a1298
Yunmi Jeong <yunmiii0226@khu.ac.kr>
2025-03-17 11:01:27 +0000

    docs: 라이브러리 링킹 메커니즘 설명 추가(Makefile과 연계된 빌드과정, 정적 라이브러리 사용방법)

--- labs/01-development-tools-in-unix/hello.c
+++ labs/01-development-tools-in-unix/hello.c
@@ -15,6 +15,7 @@ mystrcpy.o 파일로부터 libmine.a 정적 라이브러리를 생성하고,
 메인 프로그램을 빌드할 때 이 라이브러리를 링크함.
 (-lmine은 libmine.a 라이브러리를 링크하라는 의미, -L.은 현재 디렉토리에서 라이브러리를 찾으라는 의미)
 
+
 mystrcpy 함수를 정의한 mystrcpy.c 파일을 컴파일 => mystrcpy.o 생성
 mystrcpy.o => libmine.a 생성
 libmine.a와 hello.o를 링크 => 최종 실행파일인 hello 생성
