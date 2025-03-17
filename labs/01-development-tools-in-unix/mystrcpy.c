3b1ae86f0a219a2f052162b31153863eae4b5fbb
Yunmi Jeong <yunmiii0226@khu.ac.kr>
2025-03-17 10:55:13 +0000

    docs: 널 문자(\0) 복사 로직 설명, 메모리 안전성 관련 주석 추가

--- labs/01-development-tools-in-unix/mystrcpy.c
+++ labs/01-development-tools-in-unix/mystrcpy.c
@@ -1,9 +1,14 @@
 #include <string.h>
 
+/*
+C 표준 라이브러리의 strcpy 함수 직접 구현
+*/
+
 void mystrcpy(char *dst, char *src)
 {
 	while (*src)  {
 		*dst++ = *src++;
 	}
-	*dst = *src; 
+	*dst = *src; // 널 문자(\0) 복사: 문자열은 널 문자로 끝나야 함
+	/// 마지막에 널 문자로 끝나도록 하지 않으면, 다른 함수들(printf 등)이 문자열의 끝을 찾지 못하고 메모리를 계속 일어나가다가 예상치 못한 동작을 일으킬 수 있음
 }
