fc10f37ed8e138f24fae717cf6067866edf3efce
Yunmi Jeong <yunmiii0226@khu.ac.kr>
2025-03-17 10:59:07 +0000

    docs: Segmentation Fault 발생 설명 추가 (NULL 포인터 역참조)

--- labs/01-development-tools-in-unix/bug.c
+++ labs/01-development-tools-in-unix/bug.c
@@ -2,21 +2,22 @@
 #include <string.h>
 
 /*
-bug.c 프로그램에선 의도적으로 NULL 포인터에 문자열을 copy하여 Segmentation Fault를 발생시킨다.
+의도적으로 NULL 포인터에 문자열을 copy하여 Segmentation Fault를 발생시킨다.
+NULL 포인터 역참조(dereferencing)의 예시!
 */ 
  
 int main()
 {
 	int		i;
 	double	j;
-	char	*bug = NULL;
+	char	*bug = NULL; // NULL로 초기화된 문자열 포인터 선언
 
 	for (i = 0 ; i < 5 ; i++) {
 		j = i/2 + i;
 		printf("j is %lf \n", j );
 	}
 
-	strcpy(bug, "hi");
+	strcpy(bug, "hi"); /// bug는 NULL 포인터로, 그 어떤 유효한 메모리 영역도 가리키지 않는데 여기에 "hi" 문자열을 복사하려고 시도함. => 유효하지 않은 메모리 접근 (Segmentation Fault 발생)
 	printf("bug is %s \n", bug);
 
 	return 0;
